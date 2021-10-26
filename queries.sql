292e4be428d2bb8a798ecd8332b0ba8644bebfce
Diego Solis <disc3110@gmail.com>
2021-10-26 17:09:19 -0500

    create animals table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,37 @@
-/*Queries that provide answers to the questions from all projects.*/
+SELECT *
+ FROM animals
+ WHERE name ~ '[A-Za-z]+mon';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT name
+ FROM animals
+ WHERE date_of_birth 
+ BETWEEN '1-1-2016' 
+ AND '12-31-2019';
+
+ SELECT name
+ FROM animals
+ WHERE neutered = TRUE
+ AND escape_attempts < 3;
+
+ SELECT date_of_birth
+ FROM animals
+ WHERE name = 'Agumon'
+ OR name = 'Pikachu';
+
+ SELECT name, escape_attempts
+ FROM animals
+ WHERE weight_kg > 10.5;
+
+ SELECT *
+ FROM animals
+ WHERE neutered;
+
+ SELECT *
+ FROM animals
+ WHERE NOT name = 'Gabumon';
+
+ SELECT *
+ FROM animals
+ WHERE weight_kg
+ BETWEEN 10.4 
+ AND 17.3;
